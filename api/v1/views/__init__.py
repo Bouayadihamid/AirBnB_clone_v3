b42b6ef1614b0b9b9db177a0d2dcb3db1abe8623
abdelaaziz11 <abdelkhouda055@gmail.com>
2024-04-29 08:43:24 -0700

    task 8 solved

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -2,8 +2,8 @@
 """sharing app_views Blueprint"""
 
 from flask import Blueprint
-from api.v1.views.index import app_views
 app_views = Blueprint('app_views', __name__)
+from api.v1.views.index import app_views
 from api.v1.views.users import *
 from api.v1.views.amenities import *
 from api.v1.views.cities import *

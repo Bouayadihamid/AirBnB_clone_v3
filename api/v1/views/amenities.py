a486bc12c206907814c54aed648733e3655a1fb9
abdelaaziz11 <abdelkhouda055@gmail.com>
2024-04-29 08:37:54 -0700

    task 8 solved

--- api/v1/views/amenities.py
+++ api/v1/views/amenities.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
-""" A new view for Amenity objects that handles all default RESTFul API actions"""
+"""
+A new view for Amenity objects that handles all default RESTFul API actions
+"""
 
 from api.v1.views import app_views
 from models import storage
